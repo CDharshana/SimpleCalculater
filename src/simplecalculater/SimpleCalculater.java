d8d98dd11420db101d83c0465121ad9c8285450e
CDharshana <34434304+CDharshana@users.noreply.github.com>
2018-03-20 23:46:37 +0530

    First Commit

---
+++ src/simplecalculater/SimpleCalculater.java
@@ -0,0 +1,18 @@
+
+package simplecalculater;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SimpleCalculater {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculator c=new Calculator();
+        c.setVisible(true);
+    }
+    
+}
